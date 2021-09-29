9591f19a48aa7fa45d01a4805e5c1e478fc3457e
AndrewJBateman <gomezbateman@yahoo.com>
2021-09-29 19:56:01 +0200

    feat: pylint test exercise added

---
+++ Section10-Pylint/cap.py
@@ -0,0 +1,8 @@
+
+def cap_text(text):
+	'''
+	Input string
+	Output capitalised string
+  Only first letter of string is made uppercase, all other letters are made lower case
+	'''
+	return text.capitalize()
